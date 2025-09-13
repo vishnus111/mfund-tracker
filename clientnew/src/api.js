0bad9f4f8dc4c172174bcc7cc35efecf51ba0e36
vishnus111 <vishnusudeesh390@gmail.com>
2025-09-13 16:16:51 +0530

    url

--- clientnew/src/api.js
+++ clientnew/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8080/api" });
+const API = axios.create({ baseURL: " https://mfund-tracker.onrender.com/api" });
+
+// http://localhost:8080/api 
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
